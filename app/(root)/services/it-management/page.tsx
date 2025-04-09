afcb35b1fccc486ec8577854edbf136a5b4c4f06
misanthropic-codes <airabhishek098@gmail.com>
2025-04-09 14:25:40 +0530

    added page

---
+++ app/(root)/services/it-management/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NixSoftHero from "./sections/hero"
+import ServicesPage from "./sections/Services"
+
+export default function Home() {
+  return (
+    <div className="min-h-screen py-[100px] bg-gradient-to-br from-navy-900 via-blue-900 to-blue-800">
+      <NixSoftHero />
+      <ServicesPage />
+    </div>
+  );
+}
