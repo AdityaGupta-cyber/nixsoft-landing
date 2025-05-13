392c393d5109a9ea1989c5c70e23d9669d638263
misanthropic-codes <airabhishek098@gmail.com>
2025-05-13 14:32:03 +0530

    updated page

---
+++ app/(root)/solutions/observability/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import Hero from "./sections/Hero";
+import Features from "./sections/Features";
+import Benefits from "./sections/Benefits";
+import HowItWorks from "./sections/HowItWorks";
+
+
+export default function ObservabilityPage() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Hero />
+      <Features />
+      <Benefits />
+      <HowItWorks />
+     
+    </div>
+  );
+}
